b72f6eba4bc12d023156d67c46bd4bc7b74493f1
MrigortBr <igortbr0412@hotmail.com>
2026-02-17 03:29:52 -0300

    Page index defined

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,49 @@
-import Index from "@/components/switchTheme/component";
+"use client";
 
-export default function Home() {
+import { useEffect, useRef, useState } from "react";
+import VisualHeaderComponent from "@/components/header/component";
+import ActivityBarComponent from "@/components/ActivityBar/component";
+import SideBarComponent from "@/components/SideBar/component";
+import FileBar from "@/components/FileBar/component";
+import FooterComponent from "@/components/Footer/component";
+import PagesComponent from "@/components/pagesComponent/component";
+import { Pages } from "@/data/page";
+import LoadingComponent from "@/components/spinnerLoading/component";
+import { VisualMain } from "./style";
+
+export default function Index() {
+  const [pageIndex, setpageIndex] = useState(0);
+  const [isMounted, setIsMounted] = useState(false);
+  const refSystem = useRef<(HTMLDivElement | null)[]>([]);
+
+  useEffect(() => {
+    Pages.map(() => refSystem.current.push(null));
+    const update = () => {
+      setIsMounted(true);
+    };
+    update();
+  }, []);
+
+  useEffect(() => {
+    if (refSystem.current[pageIndex]) {
+      refSystem.current[pageIndex].scrollIntoView({ behavior: "smooth" });
+    }
+  }, [pageIndex]);
+
+  if (!isMounted) return <LoadingComponent></LoadingComponent>;
+
   return (
-    <div>
-      <Index></Index>
-    </div>
+    <VisualMain>
+      <VisualHeaderComponent />
+      <ActivityBarComponent />
+      <SideBarComponent pageIndex={pageIndex} setPageIndex={setpageIndex} />
+      <FileBar pageIndex={pageIndex} />
+      <PagesComponent
+        refs={refSystem}
+        pageIndex={pageIndex}
+        setPageIndex={setpageIndex}
+      />
+      <FooterComponent />
+    </VisualMain>
   );
 }
